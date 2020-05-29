32a24001085a1ef8c8bfe61f8d1b9a08bce696ed
Abhishek <abhishek7gg7@gmail.com>
2020-05-29 20:01:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const axios = require("axios");
 
 (async function() {
-var browser = puppeteer.launch({args: ['--no-sandbox']});
+var browser = await puppeteer.launch({args: ['--no-sandbox']});
 const page = await browser.newPage();
 await page.goto("https://is.gd/xxxxp");
 console.log(page.url());

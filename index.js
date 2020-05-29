f8b98c0feec9aeb666ba0213e97ee3dbb13c00e3
Abhishek <abhishek7gg7@gmail.com>
2020-05-29 20:16:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,65 +2,9 @@ const puppeteer = require('puppeteer');
 const axios = require("axios");
 
 (async function() {
-var browser = await puppeteer.launch({args: ['--no-sandbox']});
-const page = await browser.newPage();
-page.on('dialog', async dialog => {
- await dialog.accept();
-});
-   const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
-  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
-    await page.setUserAgent(userAgent);
-    await page.setViewport({
-      'width': 375,
-      'height': 812,
-      'deviceScaleFactor': 3,
-      'isMobile': true,
-      'hasTouch': true,
-      'isLandscape': false
-    });
-    await page.evaluateOnNewDocument(() => {
-  Object.defineProperty(navigator, 'webdriver', {
-    get: () => false,
-  });
-});
-    await page.evaluateOnNewDocument(() => {
-  // We can mock this in as much depth as we need for the test.
-  window.navigator.chrome = {
-    runtime: {},
-    // etc.
-  };
-});
-    await page.evaluateOnNewDocument(() => {
-  const originalQuery = window.navigator.permissions.query;
-  return window.navigator.permissions.query = (parameters) => (
-    parameters.name === 'notifications' ?
-      Promise.resolve({ state: Notification.permission }) :
-      originalQuery(parameters)
-  );
-});
-    await page.evaluateOnNewDocument(() => {
-  // Overwrite the `plugins` property to use a custom getter.
-  Object.defineProperty(navigator, 'plugins', {
-    // This just needs to have `length > 0` for the current test,
-    // but we could mock the plugins too if necessary.
-    get: () => [1, 2, 3, 4, 5],
-  });
-});
-    await page.evaluateOnNewDocument(() => {
-  // Overwrite the `plugins` property to use a custom getter.
-  Object.defineProperty(navigator, 'languages', {
-    get: () => ['en-US', 'en'],
-  });
-});
-    await page.goto("https://www.seedr.cc/");
-    await page.evaluate(function() {
-    login({username: "abhishek7gg7@gmail.com",password: "Passsw0rd"});
-    });
-    await page.waitFor(1500);
-    var m = "magnet:?xt=urn:btih:dbf21fc9a28d7c292b5cd9462683a1e150d4e0e3";
-    await page.evaluate(function(m) {
-    add_link(m)
-    },m);
-    await page.waitFor(1000);
-    await browser.close();
+  var browser = await puppeteer.launch({args: ['--no-sandbox']});
+  const page = await browser.newPage();
+  await page.goto("https://theabbie.github.io");
+  await page.screenshot({path: 'screenshot.png'});
+  await browser.close();
 })();

f66d1cd08a22b9844b01e64f44665bc00e2cb4b6
Abhishek <abhishek7gg7@gmail.com>
2020-05-29 20:32:44 +0530

    [skip ci]

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const puppeteer = require('puppeteer-core');
+const chrome = require('chrome-aws-lambda');
 const axios = require("axios");
 
 (async function() {
-  var browser = await puppeteer.launch({args: ['--no-sandbox']});
+  const browser = await puppeteer.launch({
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless
+    });
+  //var browser = await puppeteer.launch({args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.goto("https://theabbie.github.io");
   await page.screenshot({path: 'screenshot.png'});

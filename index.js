814c636613b2af5635d2e1232af64b0a425defee
Abhishek <abhishek7gg7@gmail.com>
2020-05-29 20:51:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const axios = require("axios");
 
 (async function() {
-  var browser = await puppeteer.launch({args: ['--no-sandbox']});
+  var browser = await puppeteer.launch({userDataDir: './myUserDataDir', args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.goto("https://theabbie.github.io");
   await page.screenshot({path: 'screenshot.png'});

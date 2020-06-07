05119d32e87a289bb203afc1e1975edeac9b6f36
Abhishek <abhishek7gg7@gmail.com>
2020-06-07 19:12:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const puppeteer = require('puppeteer');
 const axios = require("axios");
+const fs = require("fs");
+const util = require("util");
+const save = util.promisify(fs.writeFile);
 
 (async function() {
   var browser = await puppeteer.launch({userDataDir: './myUserDataDir', args: ['--no-sandbox']});
   const page = await browser.newPage();
-  await page.goto("https://theabbie.github.io");
-  await page.screenshot({path: 'screenshot.png'});
+  await page.goto("https://www.picuki.com/following/sasta_abbie");
+  await page.evaluate(function() {
+    setInterval(function() {
+      window.scrollTo(0,document.body.scrollHeight);
+    },100);
+  });
+  await page.waitFor(25000);
+  await save("/code.txt",page.html());
   await browser.close();
 })();

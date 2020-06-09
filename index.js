ce309e5eb011d85084bc9b892501e2ce5da0210c
Abhishek <abhishek7gg7@gmail.com>
2020-06-09 18:51:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,60 @@ var urls = ["https://www.quora.com/Is-Be-YouNick-Indian-YouTube-Channel-overrate
 (async function() {
   var browser = await puppeteer.launch({args: ['--no-sandbox']});
   const page = await browser.newPage();
+
+  const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+    await page.setUserAgent(userAgent);
+    await page.setViewport({
+      'width': 375,
+      'height': 812,
+      'deviceScaleFactor': 3,
+      'isMobile': true,
+      'hasTouch': true,
+      'isLandscape': false
+    });
+    await page.evaluateOnNewDocument(() => {
+  Object.defineProperty(navigator, 'webdriver', {
+    get: () => false,
+  });
+});
+    await page.evaluateOnNewDocument(() => {
+  // We can mock this in as much depth as we need for the test.
+  window.navigator.chrome = {
+    runtime: {},
+    // etc.
+  };
+});
+    await page.evaluateOnNewDocument(() => {
+  const originalQuery = window.navigator.permissions.query;
+  return window.navigator.permissions.query = (parameters) => (
+    parameters.name === 'notifications' ?
+      Promise.resolve({ state: Notification.permission }) :
+      originalQuery(parameters)
+  );
+});
+    await page.evaluateOnNewDocument(() => {
+  // Overwrite the `plugins` property to use a custom getter.
+  Object.defineProperty(navigator, 'plugins', {
+    // This just needs to have `length > 0` for the current test,
+    // but we could mock the plugins too if necessary.
+    get: () => [1, 2, 3, 4, 5],
+  });
+});
+    await page.evaluateOnNewDocument(() => {
+  // Overwrite the `plugins` property to use a custom getter.
+  Object.defineProperty(navigator, 'languages', {
+    get: () => ['en-US', 'en'],
+  });
+});
+
   while(true) {
   var url = urls[Math.floor(urls.length*Math.random())];
   await page.goto(url);
+  await page.evaluate(() => {
+   window.scrollTo(0,document.body.scrollHeight);
+  });
+  await page.waitFor(500);
   console.log(url);
   }
   await browser.close();

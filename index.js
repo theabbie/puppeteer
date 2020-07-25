6b60eff38d029d4f36a09c7e4a8037c162e45094
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-07-25 14:46:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,57 +6,15 @@ const save = util.promisify(fs.writeFile);
 
 (async function() {
   var browser = await puppeteer.launch({args: ['--no-sandbox']});
-  var url = "https://bit.ly/37FyArO";
+  var urls = ["https://www.iitm.ac.in/faculty/cs","https://www.iitm.ac.in/faculty/ee"];
   var page = await browser.newPage();
-
- const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
-  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
-    await page.setUserAgent(userAgent);
-    await page.setViewport({
-      'width': 375,
-      'height': 812,
-      'deviceScaleFactor': 3,
-      'isMobile': true,
-      'hasTouch': true,
-      'isLandscape': false
-    });
-    await page.evaluateOnNewDocument(() => {
-  Object.defineProperty(navigator, 'webdriver', {
-    get: () => false,
-  });
-});
-    await page.evaluateOnNewDocument(() => {
-  // We can mock this in as much depth as we need for the test.
-  window.navigator.chrome = {
-    runtime: {},
-    // etc.
-  };
-});
-    await page.evaluateOnNewDocument(() => {
-  const originalQuery = window.navigator.permissions.query;
-  return window.navigator.permissions.query = (parameters) => (
-    parameters.name === 'notifications' ?
-      Promise.resolve({ state: Notification.permission }) :
-      originalQuery(parameters)
-  );
-});
-    await page.evaluateOnNewDocument(() => {
-  // Overwrite the `plugins` property to use a custom getter.
-  Object.defineProperty(navigator, 'plugins', {
-    // This just needs to have `length > 0` for the current test,
-    // but we could mock the plugins too if necessary.
-    get: () => [1, 2, 3, 4, 5],
-  });
-});
-    await page.evaluateOnNewDocument(() => {
-  // Overwrite the `plugins` property to use a custom getter.
-  Object.defineProperty(navigator, 'languages', {
-    get: () => ['en-US', 'en'],
-  });
-});
+  for (url as urls) {
   await page.goto(url);
   await page.waitFor(3500);
-  var content = await page.evaluate(() => {return document.body.innerHTML});
-  console.log(content);
+  var content = await page.evaluate(() => {
+    return document.body.innerHTML.toString().match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+  });
+  console.log(content.join("\n"));
+  }
   await browser.close();
 })();

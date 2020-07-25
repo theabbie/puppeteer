693c296b734bc71c752fc3596988dfb8660d7f37
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-07-25 15:02:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ const save = util.promisify(fs.writeFile);
 
 (async function() {
   var browser = await puppeteer.launch({args: ['--no-sandbox']});
-  var urls = ["https://www.iitm.ac.in/faculty/cs","https://www.iitm.ac.in/faculty/ee"];
+  var urls = ["https://www.nitsri.ac.in/Department/department_facultylist.aspx?nDeptID=ec","https://www.nitsri.ac.in/Department/Department_FacultyList.aspx?nDeptID=cs","https://www.nitsri.ac.in/Department/Department_FacultyList.aspx?nDeptID=c"];
   var page = await browser.newPage();
   for (url of urls) {
+  try {
   await page.goto(url);
   await page.waitFor(3500);
   var content = await page.evaluate(() => {
     return document.body.innerText.toString().match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
   });
   console.log(content.join("\n"));
+    } catch (e) {continue;}
   }
   await browser.close();
 })();

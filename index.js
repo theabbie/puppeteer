ce361da3839dd8327f56b9a380ddafca718b4a66
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-09-27 20:11:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,40 +6,13 @@ const util = require("util");
 var url = require("url");
 var devRant = require("rantscript");
 const save = util.promisify(fs.writeFile);
+var imgur = require("imgur");
 
 (async function() {
-  /*var browser = await puppeteer.launch({args: ['--no-sandbox']});
+  var browser = await puppeteer.launch({args: ['--no-sandbox']});
   var page = await browser.newPage();
-  var url = "https://devrant.com/rants/2948571/i-think-the-fact-that-even-apple-cant-unlock-your-phone-if-you-forget-your-passc";
-  await page.goto(url);
-  await page.waitFor(3500);
-  var content = await page.evaluate(() => {
-    return document.body.textContent.toString().toLowerCase();
-  });
-  var ctr = {};
-  content.split(" ").forEach(x => ctr[x] = (ctr[x] || 0) + 1);
-  var count = [];
-  for (t in ctr) count.push([t,ctr[t]]);
-  console.log(count.sort((a,b) => (+b[1]) - (+a[1])).map(l => l[0]+": "+l[1]).join("\n"));
-  await browser.close();*/
-  var rants = [];
-  var maps = ["https://devrant.com/static/devrant/sitemaps/current/devrant_com_sitemap.xml","https://devrant.com/static/devrant/sitemaps/current/devrant_com_sitemap_2.xml","https://devrant.com/static/devrant/sitemaps/current/devrant_com_sitemap_3.xml","https://devrant.com/static/devrant/sitemaps/current/devrant_com_sitemap_4.xml","https://devrant.com/static/devrant/sitemaps/current/devrant_com_sitemap_5.xml"]
-  
-  for (p of maps) {
-     var k = cheerio.load((await axios(p,{headers: {"User-Agent": "Googlebot-News"}})).data,{xmlMode: true})
-     rants = [...rants, ...k("loc").map((i,x)=>+url.parse(k(x).text(),true).pathname.split("/")[2]).get()]
-  }
-  
-  var i = 1;
-  var token = (await devRant.login('-assmaster', process.argv[2]))["auth_token"];
-  
-  for (rant of rants) {
-     try {
-     await devRant.vote(1,rant,token);
-     console.log(rant)
-     }
-     catch(e) {
-        continue;
-     }
-  }
+  await page.goto("https://theabbie.github.io");
+  await page.screenshot({path: 'ss.png'});
+  var link = (await imgur.uploadFile('ss.png')).data.link;
+  console.log(link);
 })();

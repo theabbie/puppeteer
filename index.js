0a6e1e70b110085a8bc5e695bcf92041487b1b3a
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-09-27 20:28:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ var imgur = require("imgur");
 (async function() {
   var browser = await puppeteer.launch({args: ['--no-sandbox']});
   var page = await browser.newPage();
-  await page.goto("https://theabbie.github.io");
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36');
+  await page.goto('https://www.quora.com');
+  await page.type('input[tabindex="1"][name="email"]', "assmaster@srvrr.tk");
+  await page.type('input[tabindex="2"][name="password"]', process.argv[2]);
+  await page.keyboard.press('Tab');
+  await page.keyboard.press('Enter');
+  await page.waitForNavigation();
+  await page.click('a.deny_push_notif-button');
   await page.screenshot({path: 'ss.png'});
   var link = (await imgur.uploadFile('ss.png')).data.link;
   console.log(link);

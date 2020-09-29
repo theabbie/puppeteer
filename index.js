ca14f9e816e80b122594e7e238c1a2f493cc2d06
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-09-29 17:57:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ var imgur = require("imgur");
   await page.keyboard.press('Tab');
   await page.keyboard.press('Enter');*/
     
-  var q = "inspirational quotes";
+  var q = "If X/10+29=39, what is the value of x";
     
   await page.goto('https://google.com/search?q='+q+" quora", {
     waitUntil: 'networkidle0',
